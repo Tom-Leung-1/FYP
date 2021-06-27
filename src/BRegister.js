069bb5b03d8261c7048e463fb4199c8f0d72c01e
楓LOKED <62502174+LOKaEDe@users.noreply.github.com>
2021-06-27 17:00:51 +0800

    validation and some little parts not finish

--- src/BRegister.js
+++ src/BRegister.js
@@ -35,7 +35,7 @@ class BRegister extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-                       showPass: false 
+                       Valid: false 
                      };
     }
 
