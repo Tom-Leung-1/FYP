1f421c4c9dbe128005c5bda454ba52826e1fadda
Tom-Leung-bb <wfleung8@cse.cuhk.edu.hk>
2021-07-23 13:36:48 +0800

    custom icon, might need color change

--- src/components/GoogleMap/GoogleMap.jsx
+++ src/components/GoogleMap/GoogleMap.jsx
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
 import {useGoogleMap, GoogleMap, LoadScript, DrawingManager, Marker } from "@react-google-maps/api"
+import { faBus } from "@fortawesome/free-solid-svg-icons";
 import config from "../../config/config.json"
 import "./GoogleMap.css"
-
+import test from "../../images/public_black_24dp.svg"
 const key = config["REACT_APP_GOOGLE_KEY"]
 
 const center = {
@@ -24,7 +25,9 @@ class Map extends Component {
 
         }
     }
+    
     render() {
+        console.log(faBus.icon[4])
         const {setMarker, setMap, position} = this.props
         console.log("location", position)
         return (
@@ -46,6 +49,10 @@ class Map extends Component {
                         <Marker
                             position ={position}
                         />
+                        <Marker
+                            position ={{lat: 22.311680, lng: 114.168762}}
+                            icon="images/restaurant.svg"
+                        /> {/*this image path is not based on the current directory. It is based on public for some reasons!!!!*/}
                     </GoogleMap>
                 </LoadScript>
             </div>
