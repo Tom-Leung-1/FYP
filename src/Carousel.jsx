1dc24dc5484d92904551a82b4a38339f0bc4e2d5
楓LOKED <62502174+LOKaEDe@users.noreply.github.com>
2021-09-20 14:00:01 +0800

    Update Carousel.jsx

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import "./Carousel.css";
-import { Carousel } from 'react-responsive-carousel';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel'; //npm install react-bootstrap bootstrap@4.6.0
+import './Carousel.css';
 
 class CutsomCarousel extends Component {
     render() {
         return (
-            <Carousel >
-                <div className = "slide">
-                    <img src="images/1.jpg" />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div className = "slide"> 
-                    <img src="images/2.jpg" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div className = "slide">
-                    <img src="images/3.jpg" />
-                    <p className="legend">Legend 3</p>
-                </div>
+            <>
+            <div>
+            <Carousel fade>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="images/1.jpg"
+                />
+                <Carousel.Caption>
+                  <h3>First slide</h3>
+                  <p>Testing...</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="images/2.jpg"
+                />
+                <Carousel.Caption>
+                  <h3>Second slide</h3>
+                  <p>Testing...</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="images/3_crop3.jpg"
+                />
+                <Carousel.Caption>
+                  <h3>Third slide</h3>
+                  <p>Testing...</p>
+                </Carousel.Caption>
+              </Carousel.Item>
             </Carousel>
+          </div>
+          </>
         );
     }
 };
