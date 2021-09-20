bce15d5ecd1e195505aba9e02e16b8b98614b87f
楓LOKED <62502174+LOKaEDe@users.noreply.github.com>
2021-09-20 13:51:36 +0800

    Update SearchBox.jsx

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import {Button} from "./Button";
+//import {Button} from "./Button";
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import "./SearchBox.css";
 
 export const SearchBox = (props) => {
     return (
-        <div className="search-wrapper"> 
-            <label>Find Restaurant!</label>
-            <div>
-            <input type="text" placeholder="Search"/>
-            <Button buttonSize="btn-large">Search</Button>
-            </div>
+        <>
+        <div className="boxBG d-flex justify-content-center">
+        <div className="homeSearch m-2 p-2">
+        <div className="input-group bg-light rounded rounded-pill shadow-sm">
+          <input type="search" placeholder="Find Restaurant!" aria-describedby="button-addon1" className="form-control rounded rounded-pill border-0 bg-light m-2"/>
+          <div className="input-group-append">
+            <button id="button-addon1" type="submit" className="btn btn-link rounded rounded-pill m-2"><i className="fa fa-search"></i></button>
+          </div>
         </div>
+        </div>
+        </div>
+        </>
+        
     );
 }
