87165557c08eb2e727efe6a3013df9de0be894b9
楓LOKED <aya125416290@gmail.com>
2021-11-11 21:30:31 +0800

    Update MealCard.jsx
    pic margin change to add space btw words and pic

--- src/components/MealOrder/MealCard.jsx
+++ src/components/MealOrder/MealCard.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function MealCard({name, price, avalibleTime, maxOrder, imgSrc, addOnClick}) {
     return (
         <div class="card mb-1" id={`mealCard${name}`}>                  
-            <div class="card-body">
-                <img src={imgSrc} class="rounded img-fiuld mr-3" style={{float:"left", width:120, height:120}}/>		
+            <div class="card-body mt-1">
+                <img src={imgSrc} class="rounded img-fiuld mx-3" style={{float:"left", width:120, height:120}}/>		
                 <h3 class="card-title">{name}</h3>
                 <h5 class="card-text text-danger">${price}</h5>
                 <p class="card-text"><small class="text-muted">Avalible Time: {avalibleTime}</small></p>
