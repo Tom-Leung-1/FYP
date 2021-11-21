641e3c60cb756afb47dbc32b161319b11d5d952e
楓LOKED <aya125416290@gmail.com>
2021-11-21 17:16:54 +0800

    Booking time

--- src/BookingSetting.js
+++ src/BookingSetting.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 
 class BookingSetting extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { 
+                        weekday: "",
+                        start: "",
+                        end: "",
+                        all: "",
+                     };
+      }
+
     weekdayOpitions = () => {
         var arr = [];
         const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -11,8 +21,8 @@ class BookingSetting extends Component {
         for (let i = 0; i < 7; i++) {
           arr.push(
               <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
-                  <label class="form-check-label" for="inlineCheckbox1">{weekday[i].substring(0,3)}</label>
+                  <input class="form-check-input" type="checkbox" id="checkbox" name="weekday" value={weekday[i].substring(0,3)} onClick={()=>this.weekdayStr()} />
+                  <label class="form-check-label" for="checkbox">{weekday[i].substring(0,3)}</label>
               </div>
           );
         }
@@ -20,22 +30,95 @@ class BookingSetting extends Component {
         return arr;
     }
 
+    weekdayStr = () => {
+        let weekday = document.getElementsByName("weekday");
+        let str = "";
+        for (let i = 0; i < weekday.length; i++) {
+            if (weekday[i].checked) 
+            {
+                let j = i;
+                while (weekday[j].checked)
+                {
+                    j++;
+                    if (j >= 7) break;
+                }
+                if (j !== i+1)
+                {
+                    str += weekday[i].value + "-" + weekday[j-1].value + ",";
+                    i = j-1;
+                }
+                    
+                else
+                   str += weekday[i].value + ",";
+                
+            }
+
+        }
+        str = str.slice(0,-1);
+        this.setState({weekday: str});
+      }
+
+      TimeChange() {
+        let startT = document.getElementById("start").value;
+        let endT = document.getElementById("end").value;
+        if (endT !== "" & endT < startT)
+        {
+            alert("End time should larger than start time!");
+            this.setState({start: ""});
+            this.setState({end: ""});
+        }
+        else
+        {
+            this.setState({start: startT});
+            this.setState({end: endT});
+        }
+      }
+
+      combine() {
+          this.setState({all: this.state.weekday + " " +this.state.start + "-" + this.state.end});
+      }
+
+      checkInput () {
+          this.combine();
+          if (this.state.weekday.length === 0)
+          {
+              alert("Please choose at least one avalible weekaday!");
+              return
+          }
+          if (this.state.start.length === 0)
+          {
+              alert("Please input the start time of the avalible time range!");
+              return
+          }
+          if (this.state.end.length === 0)
+          {
+              alert("Please input the end time of the avalible time range!");
+              return
+          }
+          alert("OK");
+          let path = `OwnerOption`;
+          this.props.history.push(path);
+
+      }
+
     render() {
         return (
             <>
             <div className="container p-2">
                 <h2 className="fw-normal mt-3"><strong>Reservation Setting</strong></h2>
                 <hr/>
+                {this.state.weekday} {this.state.start}{this.state.start ? this.state.end ? "-" : "" : ""}{this.state.end}
+                <br/>
 
                 <h5 className="fw-normal mt-3"><strong>Avalible Weekday</strong></h5>
                 {this.weekdayOpitions()}
                 <h5 className="fw-normal mt-3"><strong>Avalible Time Range</strong></h5>
                 <div className="input-group mb-3" style={{width:"20em"}}>
-                    <input type="time" className="form-control"/>
+                    <input type="time" value={this.state.start} className="form-control" id="start" onChange={()=>this.TimeChange()}/>
                     <span class="input-group-text">to</span>
-                    <input type="time" className="form-control"/>
+                    <input type="time" value={this.state.end} className="form-control" id="end" onChange={()=>this.TimeChange()}/>
                 </div>
-                <button type="submit" className="btn btn-primary" style={{float: "right"}}>Save</button>
+                <button type="submit" className="btn btn-primary" style={{float: "right"}} onClick={()=>this.checkInput()}>Save</button>
             </div>            
             </>
         );
