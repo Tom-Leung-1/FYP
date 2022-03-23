82439320c959c5536af5bf0b639a1271b066b4b1
楓LOKED <aya125416290@gmail.com>
2022-03-23 13:10:49 +0800

    btn alignment update

--- src/UserType.js
+++ src/UserType.js
@@ -20,26 +20,26 @@ class UserType extends Component {
                 <div className="d-flex flex-wrap justify-content-center">
                     <div className="card border-0 bg-light my-3">
                         <Link to="/ClientOption">
-                            <button type="button" className="enlargeBtn btn btn-primary btn-lg fs-4 shadow border-0 mx-5" style={{height: "18vh", width:"22vw", background: "#6E5EFE"}}>
+                            <button type="button" className="enlargeBtn btn btn-primary btn-lg fs-4 shadow border-0" style={{height: "120px", width:"290px", background: "#6E5EFE"}}>
                             <i className="bi bi-person d-flex flex-wrap justify-content-center"></i>
                             Normal Customer
                             </button>
                         </Link>
                         <br/>
-                        <p className="mx-5 d-flex fw-bold" style={{width: "22vw"}}>
+                        <p className="mx-3 d-flex fw-bold" style={{width: "290px"}}>
                             You can serach for your favourite restaurants,
                             make orders or reservations on our website!
                         </p>
                     </div>
-                    <div className="card border-0 bg-light my-3" style={{width:"50vh"}}>
+                    <div className="card border-0 bg-light my-3">
                         <Link to={owner ? "/OwnerOption" : "/register"}>
-                            <button type="button" className="enlargeBtn btn btn-primary btn-lg fs-4 shadow border-0" style={{height: "18vh", width:"22vw", background: "#6E5EFE"}}>
+                            <button type="button" className="enlargeBtn btn btn-primary btn-lg fs-4 shadow border-0" style={{height: "120px", width:"290px", background: "#6E5EFE"}}>
                             <i className="bi bi-shop d-flex flex-wrap justify-content-center"></i>
                             Restaurant Owner
                             </button>
                         </Link>
                         <br/>
-                        <p className="mx-3 d-flex fw-bold" style={{width: "22vw"}}>
+                        <p className="mx-3 d-flex fw-bold" style={{width: "290px"}}>
                             After you submit your information about your restaurant,
                             <br/>
                             You can use our website to manage orders and reservations of your restaurant!
